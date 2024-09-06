97917683d8aa5e18717736d206c49a04b7d97e03
Artem Gusev <tigtok534@gmail.com>
2024-09-06 10:37:06 +0300

    pre-release

---
+++ README.md
@@ -0,0 +1,27 @@
+# News Demo starter files
+
+Starter files for the News application built with Go. Tutorial:
+https://freshman.tech/web-development-with-go/
+
+Here's what the [completed application](https://freshman-news.herokuapp.com/)
+looks like:
+
+![demo](https://ik.imagekit.io/freshman/news-demo_MrYio9GKlzSi.png)
+
+The code in this repo is meant to be a starting point for anyone following
+along with the tutorial.
+
+## Prerequisites
+
+You need to have [Go](https://golang.org/dl/) installed on your computer. The
+version used to test the code in the tutorial is **1.15.3**.
+
+## Get started
+
+- Clone or download this repository to your filesystem.
+
+```bash
+$ git clone https://github.com/Freshman-tech/news-demo-starter-files
+```
+
+- `cd` into the project directory and follow along with the [tutorial](https://freshman.tech/web-development-with-go/).
--- main.go
+++ main.go
@@ -1,3 +1,129 @@
 package main
 
-func handler()
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"html/template"
+	"log"
+	"math"
+	"net/http"
+	"net/url"
+	"os"
+	"strconv"
+	"time"
+)
+
+type Source struct {
+	ID   interface{} `json:"id"`
+	Name string      `json:"name"`
+}
+
+type Article struct {
+	Source      Source    `json:"source"`
+	Author      string    `json:"author"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	URL         string    `json:"url"`
+	URLToImage  string    `json:"urlToImage"`
+	PublishedAt time.Time `json:"publishedAt"`
+	Content     string    `json:"content"`
+}
+
+type Results struct {
+	Status       string    `json:"status"`
+	TotalResults int       `json:"totalResults"`
+	Articles     []Article `json:"articles"`
+}
+
+type Search struct {
+	SearchKey  string
+	NextPage   int
+	TotalPages int
+	Results    Results
+}
+
+var tpl = template.Must(template.ParseFiles("index.html"))
+
+var apiKey *string
+
+func indexHandle(w http.ResponseWriter, r *http.Request) {
+	tpl.Execute(w, nil)
+}
+
+func main() {
+	apiKey = flag.String("apikey", "", "Newsapi.org access key")
+	flag.Parse()
+
+	if *apiKey == "" {
+		log.Fatal("apiKey must be set")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3001"
+	}
+	mux := http.NewServeMux()
+	fs := http.FileServer(http.Dir("assets"))
+	mux.Handle("/assets/", http.StripPrefix("/assets", fs))
+
+	mux.HandleFunc("/search", searchHandler)
+	mux.HandleFunc("/", indexHandle)
+	http.ListenAndServe("192.168.31.31:"+port, mux)
+}
+
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+	u, err := url.Parse(r.URL.String())
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal server error"))
+		return
+	}
+
+	params := u.Query()
+	searchkey := params.Get("q")
+	page := params.Get("page")
+	if page == "" {
+		page = "1"
+	}
+
+	fmt.Println("Search Query is: ", searchkey)
+	fmt.Println("Search page is: ", page)
+
+	search := &Search{}
+	search.SearchKey = searchkey
+
+	next, err := strconv.Atoi(page)
+	if err != nil {
+		http.Error(w, "Unexpected server error", http.StatusInternalServerError)
+		return
+	}
+
+	search.NextPage = next
+	pageSize := 20
+
+	endpoint := fmt.Sprintf("https://newsapi.org/v2/everything?q=%s&pageSize=%d&page=%d&apiKey=%s&sortBy=publishedAt&language=en", url.QueryEscape(search.SearchKey), pageSize, search.NextPage, *apiKey)
+	resp, err := http.Get(endpoint)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&search.Results)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	search.TotalPages = int(math.Ceil(float64(search.Results.TotalResults) / float64(pageSize)))
+	err = tpl.Execute(w, search)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
